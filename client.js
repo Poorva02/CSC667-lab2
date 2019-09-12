48a3fca30a7b2402d658a9706d7f681ce487bdb5
Poorva <poorvarathi0208@gmail.com>
2019-09-11 20:51:21 -0700

    lab 2 submitted

--- client.js
+++ client.js
@@ -1,7 +1,22 @@
 const axios = require('axios');
 
+//axios returns promises.
+//network requests are async
+
 axios.get('http://localhost:3000')
   .then((res) => {
     console.log(res.data);
   })
-  .catch((e) => console.log(error));
+  .catch((e) => console.log(e));
+
+const body = {
+  hello: 'world',
+  anArray: [1,2,4]
+};
+
+axios.post('http://localhost:3000/post-test')
+  .then((res) => {
+  console.log(res.data);
+  })
+  .catch((e) => console.log(e));
+  
--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get('*', (req, res) => {
-  return 'hello';
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  //All logic here applies to all endpoints
+  console.log(req.originalUrl);
+  next();
 });
+
+// ``, magic keys to respond custom string
+//http://localhost:3000/poorva?hello=Poorva
+app.get('/poorva', (req, res) => {
+  res.send(`Hello, ${req.query.hello}`);
+  });
+
+app.post('/post-test', (req, res) =>{
+  res.send('worked');
+});
+
+
+//req->request, res->response
+app.get('*', (req, res) => {
+  res.send('hello, world!');
+});
+
 
 app.listen(3000);
